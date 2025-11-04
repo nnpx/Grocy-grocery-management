2f5cc694bdb44491e79f4bbe0500eeaf978e711d
Nannapas S. <nannapas.suw@gmail.com>
2025-11-04 21:53:25 +0700

    Fix: allow img of any URL

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -10,15 +10,22 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
+      //     {
+      //       protocol: 'https',
+      //       hostname: 'placehold.co',
+      //       port: '',
+      //       pathname: '/**',
+      //     },
+      //     {
+      //       protocol: 'https',
+      //       hostname: 'picsum.photos',
+      //       port: '',
+      //       pathname: '/**',
+      //     },
+      //   ],
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: '*', // ⬅️ THIS ALLOWS ANY HOSTNAME
         port: '',
         pathname: '/**',
       },
